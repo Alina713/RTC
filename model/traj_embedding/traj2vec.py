07c49c2e185b5dd1fec7dea2400949934ba32fc2
Alina713 <zagitova0622@163.com>
2023-10-30 14:56:53 +0800

    1030时间段训练+todevice

--- model/traj_embedding/traj2vec.py
+++ model/traj_embedding/traj2vec.py
@@ -6,6 +6,9 @@ import pandas as pd
 import ast
 import tqdm
 
+# 设置设备为CPU或GPU，如果有CUDA可用的话
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+
 # 定义LSTM模型类
 class LSTM(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, num_layers):
@@ -17,8 +20,8 @@ class LSTM(nn.Module):
 
     def forward(self, x):
         # 初始化隐藏状态和细胞状态
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
 
         # 通过LSTM层得到输出和最终的隐藏状态和细胞状态
         out, (hn, cn) = self.lstm(x, (h0, c0))
@@ -29,15 +32,13 @@ class LSTM(nn.Module):
 
 # 设置超参数
 input_size = 1 # 输入特征维度，例如经度、纬度、速度、方向
-output_size = 128 # 输出特征维度，例如行程时间
+output_size = 1 # 输出特征维度，例如行程时间
 hidden_size = 64 # 隐藏层大小
 num_layers = 2 # LSTM层数
 num_epochs = 5 # 训练轮数
 batch_size = 32 # 批次大小
 learning_rate = 0.01 # 学习率
 
-# 设置设备为CPU或GPU，如果有CUDA可用的话
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 加载轨迹数据集，假设已经分好了训练集和测试集，并保存为csv文件
 train_data = pd.read_csv("/nas/user/wyh/TNC/data/ETA/SHmap_train.csv", sep=';', header=0)
@@ -47,41 +48,65 @@ test_data = pd.read_csv("/nas/user/wyh/TNC/data/ETA/SHmap_test.csv", sep=';', he
 # 将字符串转换为数字列表，并计算每个序列的长度
 train_trajectory_data = [list(map(int, x.strip('[]').split(','))) for x in train_data.iloc[:,1].values]
 train_time_data = [list(map(int, x.strip('[]').split(','))) for x in train_data.iloc[:, 2].values]
+# 有-999的情况出现
+train_times_data = []
+for x in train_time_data:
+    if x[0] == -999:
+        train_times_data.append(0)
+    elif x[-1]== -999:
+        train_times_data.append(x[-2]-x[0])
+    else:
+        train_times_data.append(x[-1]-x[0])
+
 lengths = [len(x) for x in train_data.iloc[:,1].values]
-# 使用0填充到相同长度，并转换为Tensor
+# 使用-999填充到相同长度，并转换为Tensor
 max_len = max(lengths)
-train_trajectory_data = [x + [0] * (max_len - len(x)) for x in train_trajectory_data]
-train_time_data = [x + [0] * (max_len - len(x)) for x in train_time_data]
+train_trajectory_data = [x + [-999] * (max_len - len(x)) for x in train_trajectory_data]
+
 X_train = torch.tensor(train_trajectory_data).float()
-X_train = torch.narrow(X_train, dim=1, start=0, length=128)
-y_train = torch.tensor(train_time_data).float()
-y_train = torch.narrow(y_train, dim=1, start=0, length=128)
+X_train = torch.narrow(X_train, dim=1, start=0, length=128).to(device)
+y_train = torch.tensor(train_times_data).float().to(device)
+# print(y_train)
 
 # 将字符串转换为数字列表，并计算每个序列的长度
 valid_trajectory_data = [list(map(int, x.strip('[]').split(','))) for x in valid_data.iloc[:,1].values]
 valid_time_data = [list(map(int, x.strip('[]').split(','))) for x in valid_data.iloc[:, 2].values]
+valid_times_data = []
+for x in valid_time_data:
+    if x[0] == -999:
+        valid_times_data.append(0)
+    elif x[-1]== -999:
+        valid_times_data.append(x[-2]-x[0])
+    else:
+        valid_times_data.append(x[-1]-x[0])
 lengths = [len(x) for x in valid_data.iloc[:,1].values]
-# 使用0填充到相同长度，并转换为Tensor
+# 使用-999填充到相同长度，并转换为Tensor
 max_len = max(lengths)
-valid_trajectory_data = [x + [0] * (max_len - len(x)) for x in valid_trajectory_data]
-valid_time_data = [x + [0] * (max_len - len(x)) for x in valid_time_data]
+valid_trajectory_data = [x + [-999] * (max_len - len(x)) for x in valid_trajectory_data]
+
 X_valid = torch.tensor(valid_trajectory_data).float()
-X_valid = torch.narrow(X_valid, dim=1, start=0, length=128)
-y_valid = torch.tensor(valid_time_data).float()
-y_valid = torch.narrow(y_valid, dim=1, start=0, length=128)
+X_valid = torch.narrow(X_valid, dim=1, start=0, length=128).to(device)
+y_valid = torch.tensor(valid_times_data).float().to(device)
 
 # 将字符串转换为数字列表，并计算每个序列的长度
 test_trajectory_data = [list(map(int, x.strip('[]').split(','))) for x in test_data.iloc[:,1].values]
 test_time_data = [list(map(int, x.strip('[]').split(','))) for x in test_data.iloc[:, 2].values]
+test_times_data = []
+for x in test_time_data:
+    if x[0] == -999:
+        test_times_data.append(0)
+    elif x[-1]== -999:
+        test_times_data.append(x[-2]-x[0])
+    else:
+        test_times_data.append(x[-1]-x[0])
 lengths = [len(x) for x in test_data.iloc[:,1].values]
-# 使用0填充到相同长度，并转换为Tensor
+# 使用-999填充到相同长度，并转换为Tensor
 max_len = max(lengths)
-test_trajectory_data = [x + [0] * (max_len - len(x)) for x in test_trajectory_data]
-test_time_data = [x + [0] * (max_len - len(x)) for x in test_time_data]
+test_trajectory_data = [x + [-999] * (max_len - len(x)) for x in test_trajectory_data]
+
 X_test = torch.tensor(test_trajectory_data).float()
-X_test = torch.narrow(X_test, dim=1, start=0, length=128)
-y_test = torch.tensor(test_time_data).float()
-y_test = torch.narrow(y_test, dim=1, start=0, length=128)
+X_test = torch.narrow(X_test, dim=1, start=0, length=128).to(device)
+y_test = torch.tensor(test_times_data).float().to(device)
 
 # 将数据转换为张量，并划分为输入和输出
 # X_train = torch.tensor(train_data.iloc[:,1].values).float().to(device)
@@ -99,7 +124,7 @@ test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_test,
 valid_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_valid, y_valid), batch_size=batch_size, shuffle=False)
 
 # 创建LSTM模型实例，并将其移动到相应的设备上
-model = LSTM(input_size, hidden_size, output_size, num_layers)
+model = LSTM(input_size, hidden_size, output_size, num_layers).to(device)
 
 # 定义损失函数和优化器，这里使用均方误差作为损失函数，使用Adam作为优化器
 criterion = nn.MSELoss()
@@ -112,7 +137,9 @@ def evaluate(loader):
         for inputs, targets in tqdm.tqdm(loader):
             inputs = inputs.reshape(-1, inputs.size(1), input_size)
             targets = targets.reshape(-1, output_size)
+            # print(targets)
             outputs = model(inputs)
+            # print(outputs)
             loss = criterion(outputs, targets)
             total_loss += loss.item() * inputs.size(0)
             total_count += inputs.size(0)
@@ -124,10 +151,15 @@ for epoch in range(num_epochs):
     for i, (inputs, targets) in enumerate(tqdm.tqdm(train_loader)):
         # 将输入和目标调整为正确的形状，即（批次大小，序列长度，特征维度）
         inputs = inputs.reshape(-1, inputs.size(1), input_size)
+        # print(inputs)
         targets = targets.reshape(-1, output_size)
 
         # 前向传播
         outputs = model(inputs)
+        print(outputs)
+        # 断点语句
+        l=1
+        assert l==0, "error"
         loss = criterion(outputs, targets)
 
         # 反向传播和优化
